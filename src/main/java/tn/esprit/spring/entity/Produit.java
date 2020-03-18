255007b8825f62a91be67ee8a2706521cf30ec46
jawher <jawher@DESKTOP-CJTKUTI>
2020-03-19 00:14:46 +0100

    les entites produit categories et sous categories

--- src/main/java/tn/esprit/spring/entity/Produit.java
+++ src/main/java/tn/esprit/spring/entity/Produit.java
@@ -7,7 +7,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+
 
 @Entity
 @Table(name = "Produit")
@@ -42,6 +45,11 @@ public class Produit implements Serializable{
 	private Long prixAchat;
 	@Column(name = "Prix_Vente")
 	private Long prixVente;
+	
+	@ManyToOne
+	private SousCategories souscat;
+	
+	
 	public Long getId() {
 		return id;
 	}
