b067b2f6fa4497f422945ba5ff15d1b8a590cf48
Arfaoui <mohamedamine.arfaoui@esprit.tn>
2020-03-27 20:50:12 +0100

    test test 1 2 1 2 aywa aywa

--- src/main/java/tn/esprit/spring/entity/Stock.java
+++ src/main/java/tn/esprit/spring/entity/Stock.java
@@ -18,8 +18,8 @@ public class Stock implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	//@Id
+	//@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "Id_Stock")
 	private int IdStock;
 	@Column(name = "Name_Stock")
