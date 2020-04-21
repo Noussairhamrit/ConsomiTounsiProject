fbceffb4bc93c751b5012f22bcf7aa6e696fe3f9
Noussairhamrit <noussair.hamrit@esprit.tn>
2020-04-21 13:16:57 +0000

    modif

--- src/main/java/tn/esprit/spring/DAO/AppRoleDAO.java
+++ src/main/java/tn/esprit/spring/DAO/AppRoleDAO.java
@@ -8,7 +8,7 @@ import javax.persistence.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-import tn.esprit.spring.entity.UserRole;
+import tn.esprit.spring.entity.UserRole; 
  
 @Repository 
 @Transactional
